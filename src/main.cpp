32bf6225ca55637225036bab9f8b09bf7ae44d9b
mahdi <mahdi.abbasi110@ut.ac.ir>
2023-04-23 22:20:24 +0330

    bluetooth module connection added

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include<SoftwareSerial.h>
+SoftwareSerial bluetoothModule(2,3);
 
 // SHT25 I2C address is 0x40(64)
 #define Addr 0x40
+float cTemp, humidity, prev_humidity;
 
 void setup()
 {
+  bluetoothModule.begin(9600);
   // Initialise I2C communication as MASTER
   Wire.begin();
   // Initialise serial communication, set baud rate = 9600
@@ -35,7 +39,7 @@ void loop()
     data[1] = Wire.read();
 
     // Convert the data
-    float humidity = (((data[0] * 256.0 + data[1]) * 125.0) / 65536.0) - 6;
+    humidity = (((data[0] * 256.0 + data[1]) * 125.0) / 65536.0) - 6;
 
     // Output data to Serial Monitor
     Serial.print("Relative Humidity :");
@@ -62,12 +66,21 @@ void loop()
     data[1] = Wire.read();
 
     // Convert the data
-    float cTemp = (((data[0] * 256.0 + data[1]) * 175.72) / 65536.0) - 46.85;
+    cTemp = (((data[0] * 256.0 + data[1]) * 175.72) / 65536.0) - 46.85;
 
     // Output data to Serial Monitor
     Serial.print("Temperature in Celsius :");
     Serial.print(cTemp);
     Serial.println(" C");
   }
+  
+  if (isChanged(humidity)){
+    bluetoothModule.write(humidity); // send humidity
+    bluetoothModule.write(cTemp);    // send temperature
+  }
+  prev_humidity = humidity;
   delay(300);
+}
+bool isChanged(float current_humidity){
+  return (0.95 * prev_humidity > current_humidity) || (1.05 * prev_humidity < current_humidity);
 }
